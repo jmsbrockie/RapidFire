58084639280204a69f490c7915ebf6a16301dcea
James Brockie <u33jb25@abdn.ac.uk>
2026-05-13 00:25:23 +0100

    Add QR code generation and confirmation screen logic

--- root/js/script.js
+++ root/js/script.js
@@ -12,6 +12,27 @@ $(document).ready(function () {
 (e) {
         e.preventDefault();
 
+    /* QR CODE GENERATION (James) */
+
+        // Random ID 
+        const randomID = "RF-" + Math.floor(Math.random() * 90000 + 10000);
+        $('#booking-id').text(randomID);
+
+        // Clear previous QR code
+        $('#qrcode').empty();
+
+        // Generate QR code
+        new QRCode(document.getElementById("qrcode"), {
+            text: randomID,
+            width: 128,
+            height: 128,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.H
+        });
+
+        /* DETAILS/FORM SCREEN (Josh) */
+
         $("#BookingPage").hide();
         $("nav").hide();
         $("#booking-success").css("display" , "flex");
@@ -20,3 +41,10 @@ $(document).ready(function () {
 
 
 /* CONFIRMATION SCREEN (James) */
+
+$('#final-home-btn').click(function () {
+    $('#booking-success').hide();
+    $('nav').show();
+    $('section:first').css('display', 'block');
+    window.scrollTo(0, 0);
+});
