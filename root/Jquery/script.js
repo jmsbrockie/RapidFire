7079d3ddc123ef14ea8b4e4ef2016b242455389b
JoshuaLAW <joshuaaw08@gmail.com>
2026-05-12 10:23:29 +0100

    b

--- root/Jquery/script.js
+++ root/Jquery/script.js
@@ -5,5 +5,15 @@
 /* DETAILS/DESCRIPTION SCREEN (Kevin) */
 
 /* DETAILS/FORM SCREEN (Josh) */
+$(document).ready(function () {
+    $("#booking-success").hide()
+
+    $(".BookNow").on("lick" , function(e) {
+        e.preventDefault();
+        $("#BookingPage").hide()
+        $("#booking-success").show()
+    })
+})
+
 
 /* CONFIRMATION SCREEN (James) */
